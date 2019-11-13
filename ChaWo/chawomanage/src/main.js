9eb383004a56051e7340c5353691d228c2d0a09b
wyh <1398208538@qq.om>
2019-11-13 16:17:18 +0800

    随便

--- ChaWo/chawomanage/src/main.js
+++ ChaWo/chawomanage/src/main.js
@@ -1,6 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import {Row,Col,Menu, MenuItem,Badge,Button,MenuItemGroup,Submenu,Slider} from 'element-ui';
+Vue.use(Row);
+Vue.use(Col);
+Vue.use(Menu);
+Vue.use(MenuItem);
+Vue.use(Badge);
+Vue.use(Button);
+Vue.use(MenuItemGroup);
+Vue.use(Submenu);
+Vue.component(Slider.name,Slider)
 Vue.config.productionTip = false
 
 new Vue({
