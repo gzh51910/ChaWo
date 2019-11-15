6fdaa51431abd249feb35e577b2e0a387933c4dd
wyh <1398208538@qq.om>
2019-11-15 09:11:14 +0800

    后台管理系统静态页面

--- ChaWo/chawomanage/src/main.js
+++ ChaWo/chawomanage/src/main.js
@@ -1,7 +1,37 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-import {Row,Col,Menu, MenuItem,Badge,Button,MenuItemGroup,Submenu,Slider} from 'element-ui';
+import {
+  Row,
+  Col,
+  Menu,
+  MenuItem,
+  Badge,
+  Button,
+  MenuItemGroup,
+  Submenu,
+  Slider,
+  Input,
+  Dropdown,
+  DropdownMenu,
+  DropdownItem,
+  Table,
+  TableColumn,
+  Checkbox,
+  Pagination,
+  Form,
+  FormItem,
+  Select,
+  Option,
+  Switch,
+  Radio,
+  RadioGroup,
+  DatePicker,
+  TimePicker,
+  CheckboxGroup,
+  Upload,
+  Dialog,
+} from 'element-ui';
 Vue.use(Row);
 Vue.use(Col);
 Vue.use(Menu);
@@ -10,9 +40,33 @@ Vue.use(Badge);
 Vue.use(Button);
 Vue.use(MenuItemGroup);
 Vue.use(Submenu);
-Vue.component(Slider.name,Slider)
+Vue.component(Input.name, Input);
+Vue.component(Slider.name, Slider);
+Vue.use(Dropdown);
+Vue.use(DropdownMenu);
+Vue.use(DropdownItem);
+Vue.use(Table);
+Vue.use(TableColumn);
+Vue.use(Checkbox);
+Vue.use(Pagination);
+
+Vue.component(Form.name, Form);
+Vue.component(FormItem.name, FormItem);
+Vue.component(Select.name, Select);
+Vue.component(Option.name, Option);
+Vue.component(Switch.name, Switch);
+Vue.component(Radio.name, Radio);
+Vue.component(RadioGroup.name, RadioGroup);
+Vue.component(DatePicker.name, DatePicker);
+Vue.component(TimePicker.name, TimePicker);
+Vue.component(CheckboxGroup.name, CheckboxGroup);
+Vue.component(Upload.name, Upload);
+Vue.component(Dialog.name, Dialog)
 Vue.config.productionTip = false
 
+import router from './routers';
+
 new Vue({
+  router,//把路由注入实例
   render: h => h(App),
 }).$mount('#app')
---
+++ ChaWo/chawomanage/src/pages/GoodClassify.vue
@@ -0,0 +1,92 @@
+<template>
+    <div style="padding:20px">
+        <el-button type="success" icon="el-icon-plus">添加</el-button>
+        <el-button type="danger" icon="el-icon-delete">删除</el-button>
+          <el-table
+            ref="multipleTable"
+            :data="tableData"
+            tooltip-effect="dark"
+            style="width: 100%"
+            @selection-change="handleSelectionChange">
+            <el-table-column
+            type="selection"
+            width="55">
+            </el-table-column>
+            <el-table-column
+            label="添加时间"
+            width="120">
+            <template slot-scope="scope">{{ scope.row.date }}</template>
+            </el-table-column>
+            <el-table-column
+            prop="name"
+            label="商品分类"
+            width="120">
+            </el-table-column>
+            <el-table-column
+            prop="address"
+            label="添加时间"
+            show-overflow-tooltip>
+            </el-table-column>
+        </el-table>
+        <div style="margin-top: 20px">
+            <el-button @click="toggleSelection()">取消选择</el-button>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+     data() {
+      return {
+        tableData: [{
+          date: '2016-05-03',
+          name: '普洱茶',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-02',
+          name: '红茶',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-04',
+          name: '绿茶',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-01',
+          name: '乌龙茶',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-08',
+          name: '黑茶',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-06',
+          name: '白茶',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-07',
+          name: '花草茶',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }],
+        multipleSelection: []
+      }
+    },
+
+    methods: {
+      toggleSelection(rows) {
+        if (rows) {
+          rows.forEach(row => {
+            this.$refs.multipleTable.toggleRowSelection(row);
+          });
+        } else {
+          this.$refs.multipleTable.clearSelection();
+        }
+      },
+      handleSelectionChange(val) {
+        this.multipleSelection = val;
+      }
+    }
+  }
+
+</script>
+<style lang="stylus" scoped>
+
+</style>
